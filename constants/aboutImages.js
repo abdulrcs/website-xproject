7e2a2a776adc4ca9700dc4bc6b23fcf217868ecc
abdulrcs <abdulrcs1@gmail.com>
2022-10-06 14:52:50 +0700

    add media gallery

--- constants/aboutImages.js
+++ constants/aboutImages.js
@@ -5,6 +5,8 @@ const ABOUT_IMAGES = [
   'https://res.cloudinary.com/adssad/image/upload/v1661652052/image_6_wjluov.png',
   'https://res.cloudinary.com/adssad/image/upload/v1661652056/image_7_qnidtf.png',
   'https://res.cloudinary.com/adssad/image/upload/v1661652061/image_8_fuxi2g.png',
+  'https://res.cloudinary.com/adssad/image/upload/v1665041418/Bootcamp_UI_UX_xpu1nh.png',
+  'https://res.cloudinary.com/adssad/image/upload/v1665041462/Bootcamp_UI_UX_2_at0t7c.png',
 ];
 
 export default ABOUT_IMAGES;
