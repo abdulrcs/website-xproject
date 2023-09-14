fcd192a6a5c022bd6db55dbeaad5d58d467953ee
Lynt <lintangkusuma17@gmail.com>
2023-09-14 16:28:06 +0700

    Update bmc

--- src/components/championship/OverviewIC.jsx
+++ src/components/championship/OverviewIC.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ButtonPrimary from "../ButtonPrimary";
 import ButtonSecondary from "../ButtonSecondary";
 
-const OverviewIC = ({ desc, tempat, kategori, biaya, tingkat,jam,daftar,guideBook }) => {
+const OverviewIC = ({ desc, tempat, kategori, biaya,biaya2, tingkat,jam,daftar,guideBook }) => {
   return (
     <div className="w-full h-full px-6 md:px-[8.2rem] text-white text-lg md:text-2xl font-inter pb-36 relative z-20">
       <div className="border-2 w-full border-white bg-gradient-to-r from-blue-600/30 via-white/[0.03] to-blue-400/30 backdrop-blur shadow-[0px_6px_6px_0px_rgba(0,0,0,0.25)] mb-8">
@@ -24,7 +24,7 @@ const OverviewIC = ({ desc, tempat, kategori, biaya, tingkat,jam,daftar,guideBoo
         </li>
         <li>
           Biaya
-          <p className="font-normal">{biaya}</p>
+          <p className="font-normal">{biaya} <br/> {biaya2}</p>
         </li>
         <li>
           Tingkat
--- src/pages/Championship/BmcCompetition.jsx
+++ src/pages/Championship/BmcCompetition.jsx
@@ -22,23 +22,24 @@ const BmcCompetition = () => {
         <OverviewIC
           desc="Perlombaan Business Model Canvas adalah suatu kompetisi di mana peserta diminta untuk mengembangkan dan menyajikan model bisnis menggunakan kerangka kerja Business Model Canvas. Business Model Canvas adalah alat yang digunakan untuk menggambarkan dan merancang elemen-elemen kunci dari suatu model bisnis secara komprehensif dan terstruktur."
           kategori="Mahasiswa"
-           tempat="Online"
+          tempat="Online"
           jam="Via Zoom"
-          biaya="Free Pendaftaran"
+          biaya="Gel. 1 Rp 25.000"
+          biaya2 = "Gel. 2 Rp 30.000"
           tingkat="Nasional"
-          daftar = "http://unesa.me/InformaticChampionshipBMC"
-          guideBook= "http://unesa.me/GuidebookBMC"
+          daftar="http://unesa.me/InformaticChampionshipBMC"
+          guideBook="http://unesa.me/GuidebookBMC"
         />
         <SubTitleExtra title="Timeline" sub="BMC Competition" />
         <div className="pb-36">
-          <TimelineLeft nama="Pendaftaran Gel. 1 & Pembayaran" tanggal="12 September - 14 Oktober 2023" />
-          <TimelineRight nama="Pendaftaran Gel. 2 & Pembayaran" tanggal="15 Oktober - 10 November 2023" />
+          <TimelineLeft nama="Pendaftaran Gel. 1 & Pengumpulan" tanggal="12 September - 14 Oktober 2023" />
+          <TimelineRight nama="Pendaftaran Gel. 2 & Pengumpulan" tanggal="15 Oktober - 10 November 2023" />
           <TimelineLeft nama="Pengumuman Lolos Final" tanggal="17 November 2023" />
           <TimelineRight nama="Technical Meeting" tanggal="19 November 2023" />
           <TimelineLeft nama="Pengumpulan media presentasi (PPT)" tanggal="22 - 25 November 2023" />
           <TimelineRight nama="Final" tanggal="26 November 2023" />
           <TimelineLeft nama="Pengumuman Pemenang Lomba" tanggal="1 Desember 2023" />
-          
+
         </div>
         <SubTitleExtra title="The Champion Prizes" sub="BMC Competition" />
         <Prizes first="Uang Pembinaan" second="Uang Pembinaan" third="Uang Pembinaan" />
