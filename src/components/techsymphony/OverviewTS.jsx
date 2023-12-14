b5d1d99ddafd495b21f6cce3065f0828a2845eb4
Lynt <lintangkusuma17@gmail.com>
2023-12-14 20:04:06 +0700

    Typo Dikit Kgk Ngaruh

--- src/components/techsymphony/OverviewTS.jsx
+++ src/components/techsymphony/OverviewTS.jsx
@@ -9,7 +9,10 @@ const OverviewTS = ({ desc }) => {
       <div className="border-2  border-white bg-gradient-to-r from-blue-600/30 via-white/[0.03] to-blue-400/30 backdrop-blur shadow-[0px_6px_6px_0px_rgba(0,0,0,0.25)] mb-8">
         <p className="text-center py-6 px-8">{desc}</p>
       </div>
-      <SubTitleExtra title="Pembelian Ticket" sub="After Party" />
+      <div className="mt-[5rem]">
+      <SubTitleExtra title="Pembelian Ticket" sub="Techno Symphony" />
+
+      </div>
      <div className="flex flex-col md:flex-row gap-8 items-center md:justify-center">
         <ButtonPrimary text="Daftar" target="_blank" link="https://forms.gle/tvawhyEzjgZCYuAeA" />
     </div>
